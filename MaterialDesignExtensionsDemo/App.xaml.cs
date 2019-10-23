fba896ecf0fe96c4b4a1cb5373bb366804e3b9d1
mrxx99 <mrxx99@googlemail.com>
2019-10-23 22:29:34 +0200

    added ShowMeTheXaml package

--- MaterialDesignExtensionsDemo/App.xaml.cs
+++ MaterialDesignExtensionsDemo/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using ShowMeTheXAML;
+using System.Globalization;
 using System.Threading;
 using System.Windows;
 
@@ -11,6 +12,8 @@ namespace MaterialDesignExtensionsDemo
             // change the culture to test localized resource files for display strings
             /*Thread.CurrentThread.CurrentCulture = new CultureInfo("en-us");
             Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-us");*/
+
+            XamlDisplay.Init();
         }
     }
 }
